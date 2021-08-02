9b5ea3ba638dbca9d51ac1d2ced619feaf253ed0
Karrtiigehyen Veerappa <293076@via.dk>
2021-08-02 19:31:39 +0200

    Security constants, JWT Java library, JWT Token Provider, Authorization

---
+++ SupportPortalAPI/src/main/java/com/supportportal/supportportalapi/filter/JWTAuthorizationFilter.java
@@ -0,0 +1,43 @@
+package com.supportportal.supportportalapi.filter;
+
+import com.supportportal.supportportalapi.constant.SecurityConstant;
+import com.supportportal.supportportalapi.utility.JWTTokenProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter
+{
+  private JWTTokenProvider jwtTokenProvider;
+
+  public JWTAuthorizationFilter(JWTTokenProvider jwtTokenProvider)
+  {
+    this.jwtTokenProvider = jwtTokenProvider;
+  }
+
+  @Override protected void doFilterInternal(
+      HttpServletRequest httpServletRequest,
+      HttpServletResponse httpServletResponse, FilterChain filterChain)
+      throws ServletException, IOException
+  {
+    if (httpServletRequest.getMethod().equalsIgnoreCase(SecurityConstant.OPTION_HTTP_METHOD))
+    {
+      httpServletResponse.setStatus(HttpStatus.OK.value());
+    }
+    else
+    {
+      String authorizationHeader = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
+      if(authorizationHeader == null || authorizationHeader.startsWith(SecurityConstant.TOKEN_PREFIX))
+      {
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
+        return;
+      }
+    }
+  }
+}
